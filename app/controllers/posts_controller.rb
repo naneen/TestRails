e39b1f8d04a1a5d09bc9199dc94bececc2b54a79
naneen <neen_tta@hotmail.com>
2015-09-19 20:17:17 +0700

    posts can only be created by the current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,15 +25,18 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    if @post.user_id == current_user.id
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to @post, notice: 'Post was successfully created.' }
+          format.json { render :show, status: :created, location: @post }
+        else
+          format.html { render :new }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to ( @post )
     end
   end
 
